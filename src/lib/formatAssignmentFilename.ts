173d522875d001d3f4c5be248015b63c341f6ec0
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-03-01 22:56:45 +0100

    Fix dateOutput in formatAssignmentFilename function

--- src/lib/formatAssignmentFilename.ts
+++ src/lib/formatAssignmentFilename.ts
@@ -3,7 +3,7 @@ import type { AssignmentInfo } from './types';
 export function formatAssignmentFilename(values: AssignmentInfo) {
 	const { firstName, lastName, course, assignmentShortcode, date, fileType } = values;
 
-	const dateOutput = date.toString();
+	const dateOutput = date?.toString() || '';
 	const courseOutput = course.trim().toLowerCase().replace(/\s+/g, '-');
 	const assignmentShortcodeOutput = assignmentShortcode.trim().toLowerCase().replace(/\s+/g, '-');
 	const firstNameOutput = firstName.trim().toLowerCase().replace(/\s+/g, '-');
