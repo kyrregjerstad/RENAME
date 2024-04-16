0744fbf4acd902a2646782183f77c12a68b21614
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-04-16 14:25:06 +0200

    fix theme not persisting bug

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,9 @@
-import type { Theme } from '$lib/types';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-	let theme: Theme | null = null;
+	const theme = event.url.searchParams.get('theme') || event.cookies.get('colortheme') || 'dark';
 
-	const newTheme = event.url.searchParams.get('theme');
-	const cookieTheme = event.cookies.get('colortheme');
-
-	if (newTheme) {
-		theme = newTheme as Theme;
-	} else if (cookieTheme) {
-		theme = cookieTheme as Theme;
-	}
-
-	if (theme) {
-		return await resolve(event, {
-			transformPageChunk: ({ html }) => html.replace(`data-theme=""`, `data-theme="${theme}"`),
-		});
-	}
-
-	return await resolve(event);
+	return resolve(event, {
+		transformPageChunk: ({ html }) => html.replace(`data-theme=""`, `data-theme="${theme}"`),
+	});
 }) satisfies Handle;
